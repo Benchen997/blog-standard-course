63d365cc146327b53c7fee9b031448c28d07b6bb
Benchen997 <benchen.melbournechen0422@gmail.com>
2025-02-02 16:06:57 +1100

    update user provider wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Content goes here</p>
+      <div>
+        <Link href="/api/auth/login"> Login </Link>
+      </div>
+    </div>
+  )
 }
